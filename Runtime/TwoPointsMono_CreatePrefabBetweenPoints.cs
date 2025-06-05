1d86771210d683497e5d008b3abc821979a8ea0d
Eloi Stree <eloistree@gmail.com>
2025-06-06 00:27:16 +0200

    Save: Save with vertical allowed

--- Runtime/TwoPointsMono_CreatePrefabBetweenPoints.cs
+++ Runtime/TwoPointsMono_CreatePrefabBetweenPoints.cs
@@ -11,6 +11,7 @@ namespace Eloi.TwoPoints {
         public UnityEvent<GameObject> m_onPrefabCreated;
 
 
+        public float m_thresholdToBeHoriwzontal = 0.05f;
         public void SetPrefabAndCreate(GameObject toCreate)
         {
             SetPrefabToCreate(toCreate);
@@ -28,19 +29,47 @@ namespace Eloi.TwoPoints {
             {
                 return;
             }
-            Vector3 position = (m_startPoint.position + m_endPoint.position) / 2f;
 
-            Vector3 up = Vector3.up;
-            Vector3 directionX = (m_endPoint.position - m_startPoint.position);
-            Vector3 forward = Vector3.Cross(directionX,up).normalized;
-
-            Quaternion rotation = Quaternion.LookRotation(forward, Vector3.up);
-            GameObject newPrefab = Instantiate(m_prefabToCreate, position, rotation);
-            if (m_parentToCreateIn != null)
+            float yStart = m_startPoint.position.y;
+            float yEnd = m_endPoint.position.y;
+            float heightBetween = Mathf.Abs(yStart - yEnd);
+            bool isFlatHorizontal = heightBetween < m_thresholdToBeHoriwzontal;
+            if (isFlatHorizontal)
             {
-                newPrefab.transform.SetParent(m_parentToCreateIn);
+                Vector3 position = (m_startPoint.position + m_endPoint.position) / 2f;
+
+                Vector3 up = Vector3.up;
+                Vector3 directionX = (m_endPoint.position - m_startPoint.position);
+                Vector3 forward = Vector3.Cross(directionX, up).normalized;
+
+                Quaternion rotation = Quaternion.LookRotation(forward, Vector3.up);
+                GameObject newPrefab = Instantiate(m_prefabToCreate, position, rotation);
+                if (m_parentToCreateIn != null)
+                {
+                    newPrefab.transform.SetParent(m_parentToCreateIn);
+                }
+                m_onPrefabCreated?.Invoke(newPrefab);
             }
-            m_onPrefabCreated?.Invoke(newPrefab);
+            else
+            {
+                Vector3 position = (m_startPoint.position + m_endPoint.position) / 2f;
+
+                Vector3 up = Vector3.up;
+                Vector3 directionX = (m_endPoint.position - m_startPoint.position);
+                Vector3 horizontalAxis = Vector3.Cross(directionX, up).normalized;
+                Vector3 forward = Vector3.Cross(horizontalAxis, directionX).normalized;
+
+                Quaternion rotation = Quaternion.LookRotation(forward, Vector3.up);
+                rotation = rotation * Quaternion.Euler(0, 0, 90); 
+                GameObject newPrefab = Instantiate(m_prefabToCreate, position, rotation);
+                if (m_parentToCreateIn != null)
+                {
+                    newPrefab.transform.SetParent(m_parentToCreateIn);
+                }
+                m_onPrefabCreated?.Invoke(newPrefab);
+            }
+
+            
 
         }
 
